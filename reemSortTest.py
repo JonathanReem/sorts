f05ed1a33c6e5ed87765826c40334659c0c046bd
Jonathan Reem <jonathan.reem@gmail.com>
2013-09-02 20:52:38 -0400

    Added singleQuicksort to testing.

--- reemSortTest.py
+++ reemSortTest.py
@@ -1,11 +1,12 @@
 from countsort import pure_countsort, timed_countsort, countsort
 from smedSort import smedSort
 from numpySorts import np_sort, simple_np_sort
+from triPivotQuicksort import triQuicksort
 
 from compSortTest import compSortTest
 
 def main():
-	compSortTest([countsort, np_sort, sorted], verbose_timing = False)
+	compSortTest([countsort, np_sort, sorted, triQuicksort])
 
 if __name__ == '__main__':
 	main()
