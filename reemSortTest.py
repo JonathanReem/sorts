41943b746963ce8f7e94b2f8fe039dc252d9a696
Jonathan Reem <jonathan.reem@gmail.com>
2013-09-09 09:15:43 -0400

    Got rid of deprecated imports.

--- reemSortTest.py
+++ reemSortTest.py
@@ -1,12 +1,11 @@
 from countsort import pure_countsort, timed_countsort, countsort
 from smedSort import smedSort
 from numpySorts import np_sort, simple_np_sort
-from triPivotQuicksort import triQuicksort
 
 from compSortTest import compSortTest
 
 def main():
-	compSortTest([countsort, np_sort, sorted, triQuicksort], pathological=True)
+	compSortTest([countsort, np_sort, sorted])
 
 if __name__ == '__main__':
 	main()
