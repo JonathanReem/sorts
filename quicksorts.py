fb6bac490fc7b08d0bc7c3c577fc7095cdf7ca7d
Jonathan Reem <jonathan.reem@gmail.com>
2013-09-15 22:41:50 -0400

    Got rid of nonfunctioning sorts in preparation for feature finish.

--- quicksorts.py
+++ quicksorts.py
@@ -3,33 +3,6 @@ from itertools import chain
 from compSortTest import compSortTest
 import numpy as np
 
-def triQuicksort(unsorted):
-	# NON-OPERATIONAL
-	unsorted_list = unsorted[:] #Make a copy so we leave the original list alone.
-	if len(unsorted_list) < 6:
-		return sorted(unsorted_list)
-	
-	pivot_indexes = np.random.choice(range(len(unsorted_list)), size=3)
-
-	(unsorted_list[pivot_indexes[0]], unsorted_list[pivot_indexes[1]], unsorted_list[pivot_indexes[2]], 
-	   unsorted_list[0], unsorted_list[1], unsorted_list[2]) = (unsorted_list[0], unsorted_list[1], unsorted_list[2],
-	   unsorted_list[pivot_indexes[0]], unsorted_list[pivot_indexes[1]], unsorted_list[pivot_indexes[2]])
-	
-	pivots = triQuicksort(unsorted_list[:3])
-	unsorted_list = unsorted_list[3:]
-	
-	lesser1  = triQuicksort(filter(lambda num: num > pivots[0], unsorted_list))
-	lesser2  = triQuicksort(filter(lambda num: pivots[1] > num >= pivots[0], unsorted_list))
-	greater1 = triQuicksort(filter(lambda num: pivots[2] > num >= pivots[1], unsorted_list))
-	greater2 = triQuicksort(filter(lambda num: num >= pivots[2], unsorted_list))
-
-	return list(chain(lesser1, [pivots[0]], lesser2, [pivots[1]], greater1, [pivots[2]], greater2))
-
-def dualQuicksort(unsorted_list):
-	u = unsorted_list[:]
-	if len(u) < 6:
-		return sorted(u)
-
 def singleQuicksort(unsorted_list):
 	def prepPivot(u):
 		p_ind = np.random.choice(np.arange(len(unsorted_list)))
