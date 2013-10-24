bd7bc4b6900e58f8daafee91f85416cf7327ea6a
Jonathan Reem <jonathan.reem@gmail.com>
2013-10-24 15:30:24 -0400

    Changed structure, made more modular.

--- countsort.py
+++ countsort.py
@@ -18,61 +18,62 @@ def pure_countsort(unsorted_list):
 
 
 def timed_countsort(unsorted_list):
-	start_time = time.clock()
-	counts = (np.bincount(np.array(unsorted_list))).tolist()
+    start_time = time.clock()
+    counts = (np.bincount(np.array(unsorted_list))).tolist()
 
-	end_time = time.clock()
-	print "Making the count took:"
-	print end_time - start_time
-	
-	start_time = time.clock()
-	sorted_list = []
-	for num in xrange(min(unsorted_list), max(unsorted_list) + 1):
-		sorted_list += [num]*counts[num]	
-	end_time = time.clock()
-	print "Constructing the sorted list took:"
-	print end_time - start_time
-	
-	print "Total time:"
-	return sorted_list
+    end_time = time.clock()
+    print "Making the count took:"
+    print end_time - start_time
+
+    start_time = time.clock()
+    sorted_list = []
+    for num in xrange(min(unsorted_list), max(unsorted_list) + 1):
+        sorted_list += [num]*counts[num]
+    end_time = time.clock()
+    print "Constructing the sorted list took:"
+    print end_time - start_time
+
+    print "Total time:"
+    return sorted_list
 
 def test_countsort(unsorted_list):
-	"""Bleeding edge, not necessarily functional version."""
-	start_time = time.clock()
-	counts = (np.bincount(np.array(unsorted_list))).tolist()
+    """Bleeding edge, not necessarily functional version."""
+    start_time = time.clock()
+    counts = (np.bincount(np.array(unsorted_list))).tolist()
 
-	end_time = time.clock()
-	print "Making the count took:"
-	print end_time - start_time
-	
-	start_time = time.clock()
-	sorted_list = list(
-		chain.from_iterable(
-			repeat(num, counts[num])
-			for num in xrange(len(counts))))	
-	end_time = time.clock()
-	print "Constructing the sorted list took:"
-	print end_time - start_time
-	
-	print "Total time:"
-	return sorted_list
+    end_time = time.clock()
+    print "Making the count took:"
+    print end_time - start_time
+
+    start_time = time.clock()
+    sorted_list = list(
+        chain.from_iterable(
+            repeat(num, counts[num])
+            for num in xrange(len(counts))))
+    end_time = time.clock()
+    print "Constructing the sorted list took:"
+    print end_time - start_time
+
+    print "Total time:"
+    return sorted_list
 
 def countsort(unsorted_list):
-	"""Sort a list of integers (ONLY INTEGERS) in linear time. 
-	2-3x faster than sorted() for certain lists."""
-	counts = (np.bincount(np.array(unsorted_list))).tolist()
-	
-	sorted_list = list(
-		chain.from_iterable(
-			repeat(num, counts[num])
-			for num in xrange(len(counts))))
+    """Sort a list of integers (ONLY INTEGERS) in linear time.
+    2-3x faster than sorted() for certain lists."""
+    counts = (np.bincount(np.array(unsorted_list))).tolist()
 
-	return sorted_list
+    sorted_list = list(
+        chain.from_iterable(
+            repeat(num, counts[num])
+            for num in xrange(len(counts))))
+
+    return sorted_list
 
 from compSortTest import compSortTest
+import sys
 
 def main():
-	compSortTest([countsort, pure_countsort, timed_countsort, sorted], max_size_order = 7)
+    compSortTest([countsort, pure_countsort, sorted], max_size_order = int(sys.argv[1]))
 
 if __name__ == '__main__':
-	main()
+    main()
